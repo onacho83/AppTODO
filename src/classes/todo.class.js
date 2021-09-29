4069ed8252fd86bfa34b9d4976bff15a3006ba7f
OmarB <onacho83@gmail.com>
2021-09-29 08:23:33 -0300

    comienzo de la aplicacion

---
+++ src/classes/todo.class.js
@@ -0,0 +1,17 @@
+
+
+export class Todo {
+
+    constructor( tarea ){
+
+        this.tarea = tarea;
+        this.id = new Date().getTime();
+        this.creado = new Date();
+        this.completado = false;
+
+
+    }
+
+
+
+}
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import './styles.css';
+
+import {Todo} from './classes/todo.class.js';
+
+
+const tarea = new Todo('aprender JS moderno');
+console.log(tarea);
